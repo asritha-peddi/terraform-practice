5f77dea266e5bd7ac55ada1907eb8a1a87930727
Asritha <asritha.dev@gmail.com>
2025-10-31 16:42:39 +0530

    Day-3

---
+++ Day-3-statefile/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "name" {
+    instance_type = var.type
+    ami = var.ami_id
+  tags = {
+      Name = "prod" 
+  }
+}
+
+resource "aws_s3_bucket" "name" {
+    bucket = "hfsdbsdbuhhueh"
+}
---
+++ Day-3-statefile/variable.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+  description = "passing ami values"
+  type        = string
+  default     = ""
+}
+variable "type" {
+  description = "passing values to instance type"
+  type        = string
+  default     = ""
+}
